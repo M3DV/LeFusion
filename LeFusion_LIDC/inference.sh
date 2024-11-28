416479453373d53d96fae4494b49223052010b36
Yuhe Liu <yuheliu59@gmail.com>
2024-11-28 23:47:58 +0800

    Update inference.sh

--- LeFusion_LIDC/inference.sh
+++ LeFusion_LIDC/inference.sh
@@ -4,5 +4,6 @@ target_img_path=data/LIDC-IDRI/gen/Image/
 target_label_path=data/LIDC-IDRI/gen/Mask/
 jump_length=5
 jump_n_sample=5
+batch_size=2
 
-python test/inference.py test_txt_dir=$test_txt_dir dataset_root_dir=$dataset_root_dir target_img_path=$target_img_path target_label_path=$target_label_path schedule_jump_params.jump_length=$jump_length schedule_jump_params.jump_n_sample=$jump_n_sample
+python test/inference.py batch_size=$batch_size test_txt_dir=$test_txt_dir dataset_root_dir=$dataset_root_dir target_img_path=$target_img_path target_label_path=$target_label_path schedule_jump_params.jump_length=$jump_length schedule_jump_params.jump_n_sample=$jump_n_sample
